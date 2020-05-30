326c809e84521d2f7d7c4ce5de49afb5d11732a5
smfils1 <smfils1@gmail.com>
2020-05-30 10:15:35 -0400

    Heroku config fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,6 @@
 const PORT = process.env.REACT_APP_BACKEND_PORT;
-const BACKEND_URL = `http://localhost:${PORT}`;
+const BACKEND = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL =
+  process.env.NODE_ENV === "production" ? BACKEND : `http://localhost:${PORT}`;
 
 module.exports = { BACKEND_URL };
