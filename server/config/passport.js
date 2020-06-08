cf31d0f7c2cf136237633600d2af6a2bb06016df
smfils1 <smfils1@gmail.com>
2020-06-07 20:11:28 -0400

    Fixed oauth redirect url

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,8 +1,9 @@
 const passport = require("passport");
+const url = require("url");
 const GoogleStrategy = require("passport-google-oauth20");
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = require("./index");
 const User = require("../models/user/user");
-
+const config = require("./index");
 //Google Strategy
 passport.use(
   "google",
@@ -10,7 +11,7 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/redirect",
+      callbackURL: url.resolve(config.WEBSITE_URL, "/api/auth/google/redirect"),
     },
     async (accessToken, refreshToken, profile, done) => {
       //Trigger once user login. We should just save the google id and pass user object to routes (req.user)
